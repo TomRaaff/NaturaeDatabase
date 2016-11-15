521486e46d0ccd9c2fa703024ac9246384d9cdf8
Tom Raaff <tomraaff@gmail.com>
2016-11-15 12:23:49 +0100

    Clean project met spring aangemaakt

---
+++ NaturaeDatabase/src/main/java/naturaedatabase/NaturaeDatabaseApplication.java
@@ -0,0 +1,12 @@
+package naturaedatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaturaeDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaturaeDatabaseApplication.class, args);
+	}
+}
