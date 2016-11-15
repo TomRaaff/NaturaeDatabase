fb1dfaa66f9c0e0093d04c6642dd1b308fd8e4bf
Tom Raaff <tomraaff@gmail.com>
2016-11-15 15:55:25 +0100

    Eerste classes gedefinieerd en kleine PBL update

---
+++ NaturaeDatabase/src/main/java/naturaedatabase/Product.java
@@ -0,0 +1,74 @@
+package naturaedatabase;
+
+public class Product {
+	private String naam;
+	private double inkoopprijs;
+	private double verkoopprijs;
+	private double werkUren;
+	private boolean isSample;
+	private int productId;		//moet later geïnstantieerd worden op basis van de database
+	private Object klant;		
+	
+	
+	//constructor
+	public Product(){
+		
+	}
+	
+	
+	
+	//getters en setters
+	public boolean isSample() {
+		return isSample;
+	}
+
+	public String getNaam() {
+		return naam;
+	}
+
+	public void setNaam(String naam) {
+		this.naam = naam;
+	}
+
+	public double getInkoopprijs() {
+		return inkoopprijs;
+	}
+
+	public void setInkoopprijs(double inkoopprijs) {
+		this.inkoopprijs = inkoopprijs;
+	}
+
+	public double getVerkoopprijs() {
+		return verkoopprijs;
+	}
+
+	public void setVerkoopprijs(double verkoopprijs) {
+		this.verkoopprijs = verkoopprijs;
+	}
+
+	public double getWerkUren() {
+		return werkUren;
+	}
+
+	public void setWerkUren(double werkUren) {
+		this.werkUren = werkUren;
+	}
+
+	public void setSample(boolean isSample) {
+		this.isSample = isSample;
+	}
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public Object getKlant() {
+		return klant;
+	}
+
+	public void setKlant(Object klant) {
+		this.klant = klant;
+	}
+
+	
+}
---
+++ NaturaeDatabase/src/main/java/naturaedatabase/Winkel.java
@@ -0,0 +1,17 @@
+package naturaedatabase;
+
+import java.util.Date;
+
+public class Winkel {
+	private String naam;
+	private String adres;
+	private String mail;
+	private String telNr;
+	private int contractId;	//0 = geen, 1 = verhuur, 2 = consignatie, 3 = Verkoop, 4 = Trunkshow, 5 = verhuur&consignatie
+	private Date eindDatumContract;
+	private Date startDatumContract;
+	private int klantId;
+	private String notitie;
+	
+	
+}
