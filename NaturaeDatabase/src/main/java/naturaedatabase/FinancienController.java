227190d4c4218fb1f632dc687483457cbed706f3
Tom Raaff <tomraaff@gmail.com>
2016-12-12 19:45:50 +0100

    Query voor de financiën geformuleerd. Nu berekeningen maken.

--- NaturaeDatabase/src/main/java/naturaedatabase/FinancienController.java
+++ NaturaeDatabase/src/main/java/naturaedatabase/FinancienController.java
@@ -1,5 +1,11 @@
 package naturaedatabase;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.ZoneId;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class FinancienController {
 	@Autowired
-private BestellingRepository repoBestelling;
+	private BestellingRepository repoBestelling;
 	
 	@Autowired
 	private OrderlineRepository repoOrderline;
@@ -21,6 +27,17 @@ private BestellingRepository repoBestelling;
 	
 	@RequestMapping("/overzichtFinancien")
 	public String financien(Model model){
+		LocalDate datumNu = LocalDate.now(ZoneId.of("Europe/Paris"));
+		Month maandNu = datumNu.getMonth();
+		LocalDate beginMaandLD = LocalDate.of(datumNu.getYear(), maandNu, 01);
+		LocalDate eindeMaandLD = LocalDate.of(2017, maandNu.plus(1), 01);
+		Instant instantEindeMaand = eindeMaandLD.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant();
+		Instant instantBeginMaand = beginMaandLD.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant();
+		Date eindeMaand = Date.from(instantEindeMaand);
+		Date beginMaand = Date.from(instantBeginMaand);
+		System.out.println(eindeMaandLD);
+		model.addAttribute("bestellingenDezeMaand", repoBestelling.findByOpleverDatumBeforeAndOpleverDatumAfter(eindeMaand, beginMaand));
+		
 		model.addAttribute("alleBestellingen", repoBestelling.findAll());
 		model.addAttribute("alleOrderlines", repoOrderline.findAll());
 		model.addAttribute("alleKlanten", repoKlant.findAll());
